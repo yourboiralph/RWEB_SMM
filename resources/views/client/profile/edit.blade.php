b63277ccbfd256454c441f3d663e822f587a6379
Yourboiralph <hernandezralphb@gmail.com>
2025-01-31 16:43:18 +0800

    profile edit

---
+++ resources/views/client/profile/edit.blade.php
@@ -0,0 +1,64 @@
+<div class="mx-auto max-w-screen-2xl">
+    <x-navbar link="client">
+        <div class="h-full mx-auto max-w-screen-xl">
+            {{-- UPPER PART --}}
+            <x-header.upper-part name="Gabbiy" header="Profile" />
+  
+            {{-- Middle Part --}}
+            <div class="grid px-10 mt-20 grid-cols-3 h-80 gap-6 pr-32 text-black">
+              <div class="col-span-3 lg:col-span-1 bg-white shadow-md rounded-md pt-10">
+                <div class="w-full flex justify-center items-center">
+                  <img src="/Assets/user-profile-profilepage.png" alt="">
+                </div>       
+                <div class="text-center">
+                  <h1>Ga Bby</h1>  
+                </div>       
+                <div class="text-center">
+                  <h1>Bajada JP Laurel, Davao City</h1>  
+                </div>       
+              </div>
+  
+  
+              <div class="col-span-3 lg:col-span-2 bg-white shadow-md rounded-md p-5">
+                <div class="">
+                  <h1 class="text-sm">User Information</h1>
+                </div>  
+                <div class="space-y-2">
+                  <div class="flex">
+                    <div class="flex space-x-2 items-center">
+                      <img src="/Assets/name.png" class="w-5 h-4" alt="">
+                      <h1>Name</h1>
+                    </div>
+                    <input class="pl-4" value="Ga Bby">
+                  </div>
+
+                  <div class="flex">
+                    <div class="flex space-x-2 items-center">
+                      <img src="/Assets/email.png" class="w-5 h-4" alt="">
+                      <h1>Email</h1>
+                    </div>
+                    <input class="pl-4" value="Onichan@gmail.com">
+                  </div>
+
+                  <div class="flex">
+                    <div class="flex space-x-2 items-center">
+                      <img src="/Assets/phone-number.png" class="w-5 h-4" alt="">
+                      <h1>Phone</h1>
+                    </div>
+                    <input class="pl-4" value="09458941145">
+                  </div>
+
+                  <div class="flex">
+                    <div class="flex space-x-2 items-center">
+                      <img src="/Assets/address.png" class="w-5 h-4" alt="">
+                      <h1>Address</h1>
+                    </div>
+                    <input class="pl-4" value="Bajada JP Laurel, Davao City">
+                  </div>
+                </div>      
+              </div>
+            </div>
+        </div>
+    </x-navbar>
+  </div>
+  
