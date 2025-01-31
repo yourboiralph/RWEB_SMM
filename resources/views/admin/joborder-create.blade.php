6644220fc9f6893b330c25009c04ed10e6cd4b3b
Yourboiralph <hernandezralphb@gmail.com>
2025-01-31 08:11:06 +0800

    responsive

--- resources/views/admin/joborder-create.blade.php
+++ resources/views/admin/joborder-create.blade.php
@@ -5,28 +5,70 @@
     {{-- Main Content --}}
     <hr>
     <div class="m-10 px-6">
-        <form action="#">
+        <div class="w-full flex justify-between">
+            <h1 class="text-[#fa7011] font-bold text-2xl">Create Job Order</h1>
+            <div class="px-4 py-2 bg-[#fa7011] w-fit rounded-md mb-4 text-white cursor-pointer hover:bg-[#e5630f]" onclick="window.location.href='{{ route('admin.joborder') }}'">
+                Back
+            </div>  
+        </div>
+        <form action="{{url('/admin/joborder/store')}}" method="POST">
+            @csrf
             <div>
-                <label class="block">Job Order Name</label>
-                <input type="text" name="job_name" class="border p-2 w-full rounded-md">
+                <label class="block text-sm text-[#fa7011]">Job Order</label>
+                <input type="text" name="job_name" 
+                    class="border p-2 w-full rounded-md @error('project_name') border-red-500 @enderror">
+                @error('project_name')
+                    <p class="text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>            
+            <div class="mt-4">
+                <label class="block text-sm text-[#fa7011]">Job Order Description</label>
+                <textarea name="description" id="description" class="border p-2 w-full h-auto rounded-md resize-none max-h-96 overflow-y-auto @error('description') border-red-500 @enderror"></textarea>
+                @error('description')
+                    <p class="text-red-500 text-sm">{{$message}}</p>
+                @enderror
             </div>
             <div class="mt-4">
-                <label class="block">Job Order Details</label>
-                <textarea name="job_description" class="border p-2 w-full rounded-md resize-none"></textarea>
+                <label class="block text-sm text-[#fa7011]">Worker</label>
+                <p id="selected-client">No worker selected</p>
+                <input type="hidden" name="worked_by" id="client-id">
+                <div
+                    class="w-fit px-4 py-2 bg-[#fa7011] cursor-pointer rounded-lg text-white"
+                    id="open-modal"
+                >
+                    Select Worker
+                </div>
+                @error('client_id')
+                    <p class="text-red-500 text-sm">{{$message}}</p>
+                @enderror
             </div>
             <div class="mt-4">
-                <label class="block">Worked by</label>
-                <select name="worked_by" class="border p-2 w-full rounded-md">
-                    <option value="">Select Employee</option>
-                    <option value="employee1">Employee 1</option>
-                    <option value="employee2">Employee 2</option>
-                    <option value="employee3">Employee 3</option>
-                </select>
+                <label class="block text-sm text-[#fa7011]">Project</label>
+                <p id="selected-client">No project selected</p>
+                <input type="hidden" name="project_id" id="client-id">
+                <div
+                    class="w-fit px-4 py-2 bg-[#fa7011] cursor-pointer rounded-lg text-white"
+                    id="open-modal"
+                >
+                    Select Project
+                </div>
+                @error('client_id')
+                    <p class="text-red-500 text-sm">{{$message}}</p>
+                @enderror
             </div>
             <div class="mt-4">
-                <label class="block">Deadline</label>
-                <input type="date" class="border p-2 w-full rounded-md" name="target_date">
+                <label class="block text-sm text-[#fa7011]">Deadline</label>
+                <input type="date" class="border p-2 w-full rounded-md @error('target_date') border-red-500 @enderror" name="target_date">
+                @error('target_date')
+                    <p class="text-red-500 text-sm">{{$message}}</p>
+                @enderror
+            </div>
+            {{-- Submit Button Positioned to Lower fucking righyou ng nigga --}}
+            <div class="flex justify-end mt-6">
+                <button type="submit" class="px-6 py-2 bg-[#fa7011] text-white rounded-md">Create</button>
             </div>
         </form>
     </div>
+
+    
 </x-navbar>
--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -1,87 +1,81 @@
 <x-navbar>
           {{-- UPPER PART --}}
-        <div class="grid grid-cols-3">
-            <x-pagetitle header="Dashboard" />
-            <div class="bg-[#fa7011] rounded-bl-[40px] flex items-center justify-center gap-8 ml-8 mb-8">
-                <h1 class="text-white">Hi, Gabby</h1>
-                <img src="/Assets/user-profile.png" class="w-20" alt="" draggable="false">
-                <div>
-                    <i class="fa-solid fa-power-off text-xl bg-white px-2 py-1 rounded-lg" style="color: #fa7011;"></i>
-                </div>
-            </div>
-        </div>
+        <x-header.upper-part name="Gabby" header="Dashboard" />
+        <div class="container mx-auto max-w-screen-2xl">
     {{-- Middle Part --}}
-<div class="grid grid-cols-3">
-  <div class="col-span-2 px-8">
-      <img src="/Assets/Banner.png" alt="" draggable="false">
-  </div>
-  <div class="mr-8 flex flex-col items-center">
-      <div class="px-2 py-4 w-full overflow-hidden rounded-lg shadow-md bg-white">
-          <table class="table-auto w-full text-left border-collapse">
-              <thead>
-                  <tr class="text-black">
-                      <th class="">Project Development</th>
-                      <th class="">Status</th>
-                      <th class=""></th>
-                  </tr>
-              </thead>
-              <tbody class="">
-                  <tr class="">
-                      <td class="text-[12px] py-2">DM Caption and Graphics</td>
-                      <td class="text-[12px] py-2">Next Phase</td>
-                      <td class="text-[12px] text-orange-500 cursor-pointer py-2">Sign Now</td>
-                  </tr>
-                  <tr>
-                      <td class="text-[12px] py-2">DM Monthly Campaign Content</td>
-                      <td class="text-[12px] py-2">Waiting</td>
-                      <td class="text-[12px] text-orange-500 cursor-pointer py-2">Sign Now</td>
-                  </tr>
-              </tbody>
-          </table>
+    <div class="grid grid-cols-3">
+      <div class="col-span-2 px-8">
+          <img src="/Assets/Banner.png" alt="" draggable="false">
       </div>
-      <div class="mt-10 w-fit flex flex-col justify-center items-center shadow-lg">
-          <div class="h-56 w-72">
-              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/QF-HFO7Uop0?si=APB2sG6Xrdm-C-ct" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <div class="mr-8 flex flex-col items-center">
+          <div class="px-2 py-4 w-full overflow-hidden rounded-lg shadow-md bg-white">
+              <table class="table-auto w-full text-left border-collapse">
+                  <thead>
+                      <tr class="text-black">
+                          <th class="">Project Development</th>
+                          <th class="">Status</th>
+                          <th class=""></th>
+                      </tr>
+                  </thead>
+                  <tbody class="">
+                      <tr class="">
+                          <td class="text-[12px] py-2">DM Caption and Graphics</td>
+                          <td class="text-[12px] py-2">Next Phase</td>
+                          <td class="text-[12px] text-orange-500 cursor-pointer py-2">Sign Now</td>
+                      </tr>
+                      <tr>
+                          <td class="text-[12px] py-2">DM Monthly Campaign Content</td>
+                          <td class="text-[12px] py-2">Waiting</td>
+                          <td class="text-[12px] text-orange-500 cursor-pointer py-2">Sign Now</td>
+                      </tr>
+                  </tbody>
+              </table>
           </div>
-          <div class="h-20 w-72 bg-white flex items-center justify-center gap-8">
-              <i class="fa-brands fa-facebook-f px-3 py-2 rounded-full bg-[#fa7011]" style="color: #ffffff;"></i>
-              <i class="fa-brands fa-instagram px-3 py-2 rounded-full bg-[#fa7011]" style="color: #ffffff;"></i>
-              <i class="fa-brands fa-pinterest-p px-3 py-2 rounded-full bg-[#fa7011]" style="color: #ffffff;"></i>
+          <div class="mt-10 w-fit flex flex-col justify-center items-center shadow-lg">
+              <div class="h-56 w-72">
+                  <iframe width="100%" height="100%" src="https://www.youtube.com/embed/QF-HFO7Uop0?si=APB2sG6Xrdm-C-ct" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              </div>
+              <div class="h-20 w-72 bg-white flex items-center justify-center gap-8">
+                  <i class="fa-brands fa-facebook-f px-3 py-2 rounded-full bg-[#fa7011]" style="color: #ffffff;"></i>
+                  <i class="fa-brands fa-instagram px-3 py-2 rounded-full bg-[#fa7011]" style="color: #ffffff;"></i>
+                  <i class="fa-brands fa-pinterest-p px-3 py-2 rounded-full bg-[#fa7011]" style="color: #ffffff;"></i>
+              </div>
           </div>
-      </div>
-  </div>
-</div>
-
-{{-- Bottom Part --}}
-<div class="w-full mt-0">
-  <div class="carousel">
-      <div class="carousel-track">
-        <a href="https://link1.com" class="carousel-item">
-          <img src="/Assets/ads1.png" alt="Image 1" draggable="false">
-        </a>
-        <a href="https://link2.com" class="carousel-item">
-          <img src="/Assets/ads2.png" alt="Image 2" draggable="false">
-        </a>
-        <a href="https://link3.com" class="carousel-item">
-          <img src="/Assets/ads3.png" alt="Image 3" draggable="false">
-        </a>
-        <a href="https://link4.com" class="carousel-item">
-          <img src="/Assets/ads4.png" alt="Image 4" draggable="false">
-        </a>
-        <!-- Duplicate items for seamless looping -->
-        <a href="https://link1.com" class="carousel-item">
-          <img src="/Assets/ads1.png" alt="Image 1">
-        </a>
-        <a href="https://link2.com" class="carousel-item">
-          <img src="/Assets/ads2.png" alt="Image 2">
-        </a>
-        <a href="https://link3.com" class="carousel-item">
-          <img src="/Assets/ads3.png" alt="Image 3">
-        </a>
-        <a href="https://link4.com" class="carousel-item">
-          <img src="/Assets/ads4.png" alt="Image 4">
-        </a>
       </div>
     </div>
-</div>
+    
+    {{-- Bottom Part --}}
+    <div class="w-full mt-0">
+      <div class="carousel">
+          <div class="carousel-track">
+            <a href="https://link1.com" class="carousel-item">
+              <img src="/Assets/ads1.png" alt="Image 1" draggable="false">
+            </a>
+            <a href="https://link2.com" class="carousel-item">
+              <img src="/Assets/ads2.png" alt="Image 2" draggable="false">
+            </a>
+            <a href="https://link3.com" class="carousel-item">
+              <img src="/Assets/ads3.png" alt="Image 3" draggable="false">
+            </a>
+            <a href="https://link4.com" class="carousel-item">
+              <img src="/Assets/ads4.png" alt="Image 4" draggable="false">
+            </a>
+            <!-- Duplicate items for seamless looping -->
+            <a href="https://link1.com" class="carousel-item">
+              <img src="/Assets/ads1.png" alt="Image 1">
+            </a>
+            <a href="https://link2.com" class="carousel-item">
+              <img src="/Assets/ads2.png" alt="Image 2">
+            </a>
+            <a href="https://link3.com" class="carousel-item">
+              <img src="/Assets/ads3.png" alt="Image 3">
+            </a>
+            <a href="https://link4.com" class="carousel-item">
+              <img src="/Assets/ads4.png" alt="Image 4">
+            </a>
+          </div>
+        </div>
+    </div>
+        </div>
+
 </x-navbar>
