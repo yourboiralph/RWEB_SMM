60ba6641983450eafcc5d9d7bdf166c722faa938
Yourboiralph <hernandezralphb@gmail.com>
2025-02-01 11:58:34 +0800

    fixed edit and profile

--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -5,53 +5,55 @@
           <x-header.upper-part header="Profile" />
 
           {{-- Middle Part --}}
-          <div class="grid px-10 mt-10 grid-cols-3 h-80 gap-6 pr-32 text-black">
+          <div class="grid px-10 mt-10 grid-cols-3 h-80 gap-6 md:pr-32 text-black">
             <div class="px-10 col-span-3 lg:col-span-1 bg-white shadow-md rounded-md pt-10">
               <div class="w-full flex justify-center items-center">
-                <img src="/Assets/user-profile-profilepage.png" alt="">
+                <img 
+                  class="rounded-full w-32 h-32 object-cover" 
+                  src="{{ file_exists(public_path($user->image)) && $user->image ? asset($user->image) : asset('/Assets/user-profile-profilepage.png') }}" 
+                  alt="User Image">
+              </div>       
+              <div class="text-center mt-4">
+                <h1 class="font-semibold">{{$user->name}}</h1>  
               </div>       
               <div class="text-center">
-                <h1>{{$user->name}}</h1>  
-              </div>       
-              <div class="text-center">
-                <h1>{{$user->address}}</h1>  
+                <h1 class="text-gray-500">{{$user->address}}</h1>  
               </div>       
             </div>
 
-
             <div class="col-span-3 lg:col-span-2 bg-white shadow-md rounded-md p-5">
               <div class="flex justify-between">
-                <h1 class="text-sm">User Information</h1>
-                <div class="px-4 py-1 bg-[#f68e12] cursor-pointer text-white" onclick="window.location.assign('{{ route('client.profile.edit') }}')">Edit</div>
+                <h1 class="text-sm font-semibold">User Information</h1>
+                <div class="px-4 py-1 bg-[#f68e12] cursor-pointer text-white rounded-md hover:bg-[#e57f0f]" onclick="window.location.assign('{{ route('client.profile.edit') }}')">Edit</div>
               </div>  
-              <div class="space-y-2">
+              <div class="space-y-4 mt-4">
                 <div>
                   <div class="flex space-x-2 items-center">
-                    <img src="/Assets/name.png" class="w-5 h-4" alt="">
-                    <h1>Name</h1>
+                    <img src="/Assets/name.png" class="w-5 h-5" alt="">
+                    <h1 class="font-medium">Name</h1>
                   </div>
-                  <p class="pl-4">{{$user->name}}</p>
+                  <p class="pl-7 text-gray-700">{{$user->name}}</p>
                 </div>
                 <div>
                   <div class="flex space-x-2 items-center">
-                    <img src="/Assets/email.png" class="w-5 h-4" alt="">
-                    <h1>Email</h1>
+                    <img src="/Assets/email.png" class="w-5 h-5" alt="">
+                    <h1 class="font-medium">Email</h1>
                   </div>
-                  <p class="pl-4">{{$user->email}}</p>
+                  <p class="pl-7 text-gray-700">{{$user->email}}</p>
                 </div>
                 <div>
                   <div class="flex space-x-2 items-center">
-                    <img src="/Assets/phone-number.png" class="w-5 h-4" alt="">
-                    <h1>Phone</h1>
+                    <img src="/Assets/phone-number.png" class="w-5 h-5" alt="">
+                    <h1 class="font-medium">Phone</h1>
                   </div>
-                  <p class="pl-4">{{$user->phone}}</p>
+                  <p class="pl-7 text-gray-700">{{$user->phone}}</p>
                 </div>
                 <div>
                   <div class="flex space-x-2 items-center">
-                    <img src="/Assets/address.png" class="w-5 h-4" alt="">
-                    <h1>Address</h1>
+                    <img src="/Assets/address.png" class="w-5 h-5" alt="">
+                    <h1 class="font-medium">Address</h1>
                   </div>
-                  <p class="pl-4">{{$user->address}}</p>
+                  <p class="pl-7 text-gray-700">{{$user->address}}</p>
                 </div>
               </div>      
             </div>
